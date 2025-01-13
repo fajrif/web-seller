b4ba5d67ca1081caa23c0818f5d1982f190fbaba
Fajri Fachriansyah <fajri82@gmail.com>
2025-01-13 20:00:52 +0700

    remove unused components

--- components.d.ts
+++ components.d.ts
@@ -47,7 +47,6 @@ declare module 'vue' {
     ScrollToTop: typeof import('./src/@core/components/ScrollToTop.vue')['default']
     Shortcuts: typeof import('./src/@core/components/Shortcuts.vue')['default']
     Stencil: typeof import('./src/components/dialogs/vue-advanced-cropper/Stencil.vue')['default']
-    StencilBanner: typeof import('./src/components/dialogs/vue-advanced-cropper/StencilBanner.vue')['default']
     TablePagination: typeof import('./src/@core/components/TablePagination.vue')['default']
     TheCustomizer: typeof import('./src/@core/components/TheCustomizer.vue')['default']
     ThemeSwitcher: typeof import('./src/@core/components/ThemeSwitcher.vue')['default']
@@ -55,8 +54,6 @@ declare module 'vue' {
     UpdateHargaDialog: typeof import('./src/components/dialogs/UpdateHargaDialog.vue')['default']
     UpdateStatusDialog: typeof import('./src/components/dialogs/UpdateStatusDialog.vue')['default']
     UpdateStockDialog: typeof import('./src/components/dialogs/UpdateStockDialog.vue')['default']
-    UploadCropImageDialog: typeof import('./src/components/dialogs/UploadCropImageDialog.vue')['default']
-    UploadCropStencilBannerImageDialog: typeof import('./src/components/dialogs/UploadCropStencilBannerImageDialog.vue')['default']
     UploadCropStencilImageDialog: typeof import('./src/components/dialogs/UploadCropStencilImageDialog.vue')['default']
   }
 }
