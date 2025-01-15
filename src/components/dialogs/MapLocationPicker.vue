2268b1c4bec71d8df148a8d97b72dbf6cf5abe51
Fajri Fachriansyah <fajri82@gmail.com>
2025-01-15 22:28:38 +0700

    google autocomplete api

--- src/components/dialogs/MapLocationPicker.vue
+++ src/components/dialogs/MapLocationPicker.vue
@@ -51,62 +51,70 @@ const currPos = computed(() => ({
 
 watch(() => props.isDialogVisible, async (visible) => {
   if (visible) {
-		try {
-			if(coords.value){
-				initPos.lat = props.latitude == 0 ? coords.value.latitude : props.latitude
-				initPos.lng = props.longitude == 0 ? coords.value.longitude : props.longitude
-				currentPos.value.lat = initPos.lat
-				currentPos.value.lng = initPos.lng
-			}
-			// Promise for a specific library
-			loader
-				.importLibrary('maps')
-				.then(async ({ Map, InfoWindow }) => {
-					const map = new Map(mapDiv.value, {
-						center: currPos.value,
-						zoom: 17,
-						treetViewControl: false,
-            mapTypeControl: false,
-						mapId: "ALAMAT_MAP_ID"
-					});
-					const { AdvancedMarkerElement } = await loader.importLibrary('marker');
-					const draggableMarker = new AdvancedMarkerElement({ map, position: currPos.value, gmpDraggable: true });
-					draggableMarker.addListener("dragend", (event) => {
-						const position = draggableMarker.position
-						currentPos.value.lat = position.lat
-						currentPos.value.lng = position.lng
-					});
+		if(coords.value){
+			initPos.lat = props.latitude == 0 ? coords.value.latitude : props.latitude
+			initPos.lng = props.longitude == 0 ? coords.value.longitude : props.longitude
+			currentPos.value.lat = initPos.lat
+			currentPos.value.lng = initPos.lng
+		}
 
-					map.addListener("click", (event) => {
-						currentPos.value.lat = event.latLng.lat()
-						currentPos.value.lng = event.latLng.lng()
-						draggableMarker.position = { lat: currentPos.value.lat, lng: currentPos.value.lng }
-					});
-
-					// clickListener = map.value.addListener('click',
-					// 	({ latLng: { lat, lng } }) =>
-					// 		(otherPos.value = { lat: lat(), lng: lng() })
-					// )
-
-					// google.maps.event.addListener(map, 'click', function(event) {
-					// 		marker.setPosition(event.latLng);
-					// });
-
-				})
-				.catch((e) => {
-					// do something
-					console.log(e)
+		// Promise for a specific library
+		loader
+			.importLibrary('maps')
+			.then(async ({ Map }) => {
+				const map = new Map(mapDiv.value, {
+					center: currPos.value,
+					zoom: 17,
+					treetViewControl: false,
+					mapTypeControl: false,
+					mapId: "ALAMAT_MAP_ID"
 				});
-      } catch (e) {
-        console.log(e.message);
-      }
+				const { AdvancedMarkerElement } = await loader.importLibrary('marker');
+				const draggableMarker = new AdvancedMarkerElement({ map, position: currPos.value, gmpDraggable: true });
+				draggableMarker.addListener("dragend", (event) => {
+					const position = draggableMarker.position
+					currentPos.value.lat = position.lat
+					currentPos.value.lng = position.lng
+				});
+
+				map.addListener("click", (event) => {
+					currentPos.value.lat = event.latLng.lat()
+					currentPos.value.lng = event.latLng.lng()
+					draggableMarker.position = { lat: currentPos.value.lat, lng: currentPos.value.lng }
+				});
+
+				// Autocomplete API
+				const Places = await loader.importLibrary('places')
+				//this object will be our second arg for the new instance of the Places API
+				const options = {
+					componentRestrictions: { country: "id" },
+					fields: ["address_component", "geometry", "formatted_address", "name"],
+				};
+				// per the Google docs create the new instance of the import above. I named it Places.
+				const input = document.getElementById("app-text-field-search-input")
+				const autocomplete = new Places.Autocomplete(input, options);
+
+				//add the place_changed listener to display results when inputs change
+				autocomplete.addListener('place_changed', () => {
+					const place = autocomplete.getPlace(); //this callback is inherent you will see it if you logged autocomplete
+					currentPos.value.lat = place.geometry.location.lat()
+					currentPos.value.lng = place.geometry.location.lng()
+					draggableMarker.position = { lat: currentPos.value.lat, lng: currentPos.value.lng }
+				});
+
+			})
+			.catch((e) => {
+				// do something
+				console.log(e)
+			});
+
 	}
 	else {
 		// reset lat,lng
 		initPos.lat = 0
 		initPos.lng = 0
-		currentPos.lat = 0
-		currentPos.lng = 0
+		currentPos.value.lat = 0
+		currentPos.value.lng = 0
 	}
 })
 
@@ -148,9 +156,13 @@ const onSubmit = () => {
 					<VCol cols="12">
 
 						<div class="d-flex text-center mb-2">
-							<div class="mx-auto">
-								<h4>Selected Position</h4>
-								<span v-if="currPos">
+							<div class="w-100">
+								<AppTextField
+									id="search-input"
+									append-inner-icon="tabler-map-pin"
+									placeholder="Cari alamat atau lokasi ..."
+								/>
+								<span v-if="currPos" class="text-sm">
 									Latitude:  <strong>{{ currPos.lat }}</strong>
 									Longitude: <strong>{{ currPos.lng }}</strong>
 								</span>
@@ -184,3 +196,8 @@ const onSubmit = () => {
     </VCard>
   </VDialog>
 </template>
+<style>
+.pac-container {
+	z-index: 9999 !important;
+}
+</style>
